57e556995c50e974ea3b73279d779cdf98ba17bc
KJH <daeding7@naver.com>
2021-11-26 20:59:03 +0900

    양 옆의 여백 공간 제거

--- app/src/main/java/com/kim/drawablexmlpractice_kim/MainActivity.kt
+++ app/src/main/java/com/kim/drawablexmlpractice_kim/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kim.drawablexmlpractice_kim
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +21,9 @@ class MainActivity : AppCompatActivity() {
 //        실제로 어떤 커스텀 뷰를 사용할 지 설정
 
         defaultActionBar.setCustomView(R.layout.my_custom_actionbar)
+//        커스톰 뷰를 들고 있는 툴바를 변수로 지정
 
+        val toolbar = defaultActionBar.customView.parent as Toolbar
+        toolbar.setContentInsetsAbsolute(0,0)
     }
 }
